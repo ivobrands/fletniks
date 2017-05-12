5d30387f63359b469052762faac49626c22890a5
ivobrands <ivobrands@home.nl>
2017-05-12 10:03:42 +0200

    first commit with usefull data

---
+++ fletnix/Controllers/MovieController.cs
@@ -0,0 +1,205 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using fletnix.Models;
+using fletnix.Services;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace fletnix
+{
+    public class MovieController : Controller
+    {
+        private readonly fletnixContext _context;
+
+        public MovieController(fletnixContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Movie
+        public async Task<IActionResult> Index(
+            string sortOrder,
+            string currentFilter,
+            string searchString,
+            int? page)
+        {
+
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DurationSortParm"] = sortOrder == "duration" ? "duration_desc" : "duration";
+
+            ViewData["CurrentSort"] = sortOrder;
+
+            var movie = from s in _context.Movie
+                select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movie = movie.Where(m => m.Title.Contains(searchString)
+                                               || m.Description.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    movie = movie.OrderByDescending(m => m.Title);
+                    break;
+                case "duration":
+                    movie = movie.OrderBy(m => m.Duration);
+                    break;
+                case "duration_desc":
+                    movie = movie.OrderByDescending(m => m.Duration);
+                    break;
+                default:
+                    movie = movie.OrderBy(m => m.Title);
+                    break;
+            }
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+
+            int pageSize = 25;
+            return View(await PaginatedList<Movie>.CreateAsync(movie.AsNoTracking(), page ?? 1, pageSize));
+        }
+
+        // GET: Movie/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .Include(m => m.PreviousPartNavigation)
+                .SingleOrDefaultAsync(m => m.MovieId == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movie/Create
+        public IActionResult Create()
+        {
+            ViewData["PreviousPart"] = new SelectList(_context.Movie, "MovieId", "Title");
+            return View();
+        }
+
+        // POST: Movie/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MovieId,Title,Duration,Description,PublicationYear,CoverImage,PreviousPart,Price,Url")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["PreviousPart"] = new SelectList(_context.Movie, "MovieId", "Title", movie.PreviousPart);
+            return View(movie);
+        }
+
+        // GET: Movie/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.SingleOrDefaultAsync(m => m.MovieId == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            ViewData["PreviousPart"] = new SelectList(_context.Movie, "MovieId", "Title", movie.PreviousPart);
+            return View(movie);
+        }
+
+        // POST: Movie/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MovieId,Title,Duration,Description,PublicationYear,CoverImage,PreviousPart,Price,Url")] Movie movie)
+        {
+            if (id != movie.MovieId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.MovieId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            ViewData["PreviousPart"] = new SelectList(_context.Movie, "MovieId", "Title", movie.PreviousPart);
+            return View(movie);
+        }
+
+        // GET: Movie/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .Include(m => m.PreviousPartNavigation)
+                .SingleOrDefaultAsync(m => m.MovieId == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movie/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movie = await _context.Movie.SingleOrDefaultAsync(m => m.MovieId == id);
+            _context.Movie.Remove(movie);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movie.Any(e => e.MovieId == id);
+        }
+    }
+}
---
+++ fletnix/Models/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fletnix.Models
+{
+    public partial class Event
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string Location { get; set; }
+
+        public virtual Award NameNavigation { get; set; }
+    }
+}
